4611b1ab7c7beabd2d05debc6fba960d2472fa11
Brandon Ngo <okibrandon@hotmail.com>
2021-12-19 16:06:02 -0500

    Time: 48 ms (39.23%), Space: 15.8 MB (5.92%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,16 +1,40 @@
+#include <map>
+
 class Solution {
 public:
   vector<int> twoSum(vector<int>& nums, int target) {
-    int length = nums.capacity();
-    for (int x = 0; x < length; x++) {
-      for (int y = 0; y < length; y++) {
-        if (y != x) {
-          if (nums[x] + nums[y] == target) {
-            return {x, y};
-          }
-        }
-      }
+    map<int, int> indexOfNums;
+    
+    for (int i = 0; i < nums.size(); i++) {
+      indexOfNums.insert(pair<int, int>(nums[i], i));
     }
+    
+    for (int i = 0; i < nums.size(); i++) {
+       try {
+         int match = indexOfNums.at(target - nums[i]);
+         if (match == i) {
+           continue;
+         } else {
+           return {match, i};
+         }
+       } catch (const out_of_range& e) {
+         continue;
+       }
+    } 
     return {};
   }
 };
+
+
+/*
+
+2,7,11,15 = 9
+
+3 4 7 9 19 11 = 18
+
+
+
+
+
+
+*/
